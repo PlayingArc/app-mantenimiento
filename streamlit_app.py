71dd6a67efbe9b81fdfa44bcd15ede97b47590ea
PlayingArc <133719393+PlayingArc@users.noreply.github.com>
2025-03-03 12:48:48 -0600

    Update streamlit_app.py
    * Arreglé errores relacionados al cambio de año
    * Ahora se genera reporte de ulitmos 6 meses
    * El mes de trabajo por default es el mes anterior a la fecha de consulta

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,21 @@
+import re
 import streamlit as st
 import datetime
 import pandas as pd
 import altair as alt
-import re
+import datetime as dt
+
+def lista_ultimos_seis_meses(today:dt.date):
+    ##LISTA DE ULTIMOS 6 MESES EN ORDEN
+    ultimos_meses = list()
+    for mes in range(0, 6):
+        mes_anterior = today.month - 1
+        if (mes_anterior - mes) > 0:
+            ultimos_meses.append(mes_anterior - mes)
+        else:
+            ultimos_meses.append(14 - mes)
+
+    return ultimos_meses
 
 def clean_blanks(df: pd.DataFrame) -> pd.DataFrame:
     '''
@@ -20,7 +33,6 @@ def clean_blanks(df: pd.DataFrame) -> pd.DataFrame:
         df.loc[:, col] = df[col].astype('string', errors='ignore').str.strip()
 
     return df
-
 
 def preprocess_maintenance_data(df: pd.DataFrame) -> pd.DataFrame:
     '''
@@ -74,7 +86,6 @@ def preprocess_maintenance_data(df: pd.DataFrame) -> pd.DataFrame:
     df = df[orden_de_cols]
     return df
 
-
 def match_equipo(x: str):
     categs_equipo = ['ALIMENTADOR',
                      'HORNO',
@@ -94,7 +105,6 @@ def match_equipo(x: str):
         return categs_equipo[count - 1]
     else:
         return 'otro'
-
 
 def datos_por_lineas(df: pd.DataFrame, columna_equipo='equipo') -> pd.DataFrame:
     '''
@@ -130,7 +140,6 @@ def datos_por_lineas(df: pd.DataFrame, columna_equipo='equipo') -> pd.DataFrame:
 
     return df
 
-
 def data_engineering_maintenance_data(df: pd.DataFrame) -> pd.DataFrame:
     '''
     Data engineerning para dashboard de mantenimiento.
@@ -160,7 +169,7 @@ def data_engineering_maintenance_data(df: pd.DataFrame) -> pd.DataFrame:
 
     # asigno tipos de datos manualmente
     df.prioridad = df.prioridad.astype('category')
-    df['tipo_mantenimiento'] = df['tipo_mantenimiento'].map(lambda x: 'PREVENTIVO' if x == 1 else 'CORRECTIVO')
+    df['tipo_mantenimiento'] = df['tipo_mantenimiento'].map(lambda x: 'PREVENTIVO' if x == 2 else 'CORRECTIVO')
 
     # extras de fecha
     df['año'] = df.fecha_programada.dt.year
@@ -178,7 +187,6 @@ def data_engineering_maintenance_data(df: pd.DataFrame) -> pd.DataFrame:
 
     return df
 
-
 def data_prep(df: pd.DataFrame) -> pd.DataFrame:
     '''
     Prepara los datos de mantenimiento para ser graficados
@@ -189,49 +197,89 @@ def data_prep(df: pd.DataFrame) -> pd.DataFrame:
     df = data_engineering_maintenance_data(df)
     return df
 
-
 def generar_diagrama_de_calor(df: pd.DataFrame) -> alt.Chart:
     '''
     Toma los datos procesados de mantenimiento para ser graficados
     :param df: datos procesados
     :return: diagrama de calor de mantenimiento
     '''
-    hm_t = alt.Chart(df).mark_rect().encode(
-        alt.Y('month(fecha_inicial):O', title='Mes'),
-        alt.X('linea:O', title = 'Linea'),
-        alt.Color(
-            'sum(horas_reales):Q',
-            scale=alt.Scale(scheme='blues'),
-            title='Horas'
-        ),
-        stroke=alt.value('black'),
-        strokeWidth=alt.value(0.25),
-        tooltip=alt.Tooltip('sum(horas_reales):Q', )
-    ).transform_filter(
-        (alt.datum.linea != "H") & (alt.datum.linea != "n/a")
-    )
+    first = True
+    for year in df['año'].unique():
+        if first == True:
 
-    hm_h = alt.Chart(df).mark_rect().encode(
-        alt.Y('month(fecha_inicial):O', title=None),
-        alt.X('linea:O', title = 'Linea'),
-        alt.Color(
-            'sum(horas_reales):Q',
-            scale=alt.Scale(scheme='reds'),
-            title='Horas'
-        ),
-        stroke=alt.value('black'),
-        strokeWidth=alt.value(0.25),
-        tooltip=alt.Tooltip('sum(horas_reales):Q', )
-    ).transform_filter(
-        alt.datum.linea == "H"
-    )
+            hm_t = alt.Chart(df.query(f"año=={year}")).mark_rect().encode(
+                alt.Y('month(fecha_inicial):T', title='Mes'),
+                alt.X('linea:O', title = 'Linea', sort=['1','2','3','4','5','6','7','8','9','10','11','12','13']),
+                alt.Color(
+                    'sum(horas_reales):Q',
+                    scale=alt.Scale(scheme='blues'),
+                    title='Horas'
+                ),
+                stroke=alt.value('black'),
+                strokeWidth=alt.value(0.25),
+                tooltip=alt.Tooltip('sum(horas_reales):Q', )
+            ).transform_filter(
+                (alt.datum.linea != "H") & (alt.datum.linea != "n/a")
+            ).properties(title=str(year))
+
+            hm_h = alt.Chart(df.query(f"año=={year}")).mark_rect().encode(
+                alt.Y('month(fecha_inicial):T', title=None),
+                alt.X('linea:N', title = 'Linea'),
+                alt.Color(
+                    'sum(horas_reales):Q',
+                    scale=alt.Scale(scheme='reds'),
+                    title='Horas'
+                ),
+                stroke=alt.value('black'),
+                strokeWidth=alt.value(0.25),
+                tooltip=alt.Tooltip('sum(horas_reales):Q', )
+            ).transform_filter(
+                alt.datum.linea == "H"
+            ).properties(title=str(year))
+            first = False
+
+        else:
+            hm_t = hm_t.properties(height=150, width=300)
+            hm_t = (alt.Chart(df.query(f"año=={year}")).mark_rect().encode(
+                alt.Y('month(fecha_inicial):T', title='Mes'),
+                alt.X('linea:O', title='Linea',
+                      sort=['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13']),
+                alt.Color(
+                    'sum(horas_reales):Q',
+                    scale=alt.Scale(scheme='blues'),
+                    title='Horas'
+                ),
+                stroke=alt.value('black'),
+                strokeWidth=alt.value(0.25),
+                tooltip=alt.Tooltip('sum(horas_reales):Q', )
+            ).transform_filter(
+                (alt.datum.linea != "H") & (alt.datum.linea != "n/a")
+            ).properties(height=150, width=300).properties(title=str(year))
+                    & hm_t)
+
+            hm_h = hm_h.properties(height=150)
+            hm_h = (alt.Chart(df.query(f"año=={year}")).mark_rect().encode(
+                alt.Y('month(fecha_inicial):T', title=None),
+                alt.X('linea:N', title='Linea'),
+                alt.Color(
+                    'sum(horas_reales):Q',
+                    scale=alt.Scale(scheme='reds'),
+                    title='Horas'
+                ),
+                stroke=alt.value('black'),
+                strokeWidth=alt.value(0.25),
+                tooltip=alt.Tooltip('sum(horas_reales):Q', )
+            ).transform_filter(
+                alt.datum.linea == "H"
+            ).properties(height=150).properties(title=str(year))
+                    & hm_h)
 
     # junto los diagramas sin que compartan escala
     heat_correctivos = (hm_t | hm_h).resolve_scale(
         color='independent',  # Make sure y-scales are independent
     )
-
     return heat_correctivos
+
 def generar_diagrama_tiempo(df: pd.DataFrame, timeFrame: str) -> alt.Chart:
     diagrama_de_tiempo = alt.Chart(df).mark_bar().encode(
         alt.X('fecha_inicial:T',
@@ -247,7 +295,6 @@ def generar_diagrama_tiempo(df: pd.DataFrame, timeFrame: str) -> alt.Chart:
 
     return diagrama_de_tiempo
 
-
 def generar_diagrama_de_barras(df: pd.DataFrame) -> alt.Chart:
     diagra_de_barras = alt.Chart(df).mark_bar().encode(
         alt.Y('trabajador:N', title=None, sort='-x'),
@@ -256,8 +303,8 @@ def generar_diagrama_de_barras(df: pd.DataFrame) -> alt.Chart:
                   legend=None),
         tooltip=['trabajador:N', 'sum(horas_reales):Q']
     ).interactive()
-
     return diagra_de_barras
+
 def generar_pie_chart(df: pd.DataFrame, mostrar_n: int):
     datos_pie = df.groupby('tipo_tarea',
                            observed=False)['horas_reales'].count().sort_values(ascending=False)
@@ -289,22 +336,29 @@ st.set_page_config(
     page_title = "Reporte de Mantenimiento",
     page_icon = "🏂",
     layout = "wide",
-    initial_sidebar_state = "expanded")
+    initial_sidebar_state = "expanded"
+)
 
 alt.themes.enable("dark")
 
 #FILTER IN THE SIDEBAR
 
 with st.sidebar:
-    st.title('🗂 Carga de Datos️')
-    # Carga de documento
+    st.title('Carga tus datos 🗂')
     uploaded_file = st.file_uploader('Datos de reporte "Tareas por trabajador por Orden con costo"', type=['xlsx'])
 
 if uploaded_file is not None:
-
+    #PROCESAMIENTO DE DATOS
     data = pd.read_excel(uploaded_file)
     data = data_prep(data)
 
+    ##LISTA DE ULTIMOS 6 MESES EN ORDEN
+    fecha_actual = dt.datetime.today().date()
+    ultimos_seis_meses = lista_ultimos_seis_meses(fecha_actual)
+    st.title('Dashboard de Mantenimiento 🔩')
+
+
+    #SELECCIONA MES DEL REPORTE
     with st.sidebar:
         st.divider()
         st.subheader('Generar Reporte')
@@ -321,13 +375,24 @@ if uploaded_file is not None:
                           'Octubre' : 10,
                           'Noviembre' : 11,
                           'Diciembre' : 12}
+        meses_legibles_inv = {v: k for k, v in meses_legibles.items()}
 
-        mes_de_trabajo = st.selectbox(label = 'El mes de trabajo: ',
-                                      options = meses_legibles.keys())
+        mes_de_trabajo = st.selectbox(label = 'Mes',
+                                      options = [meses_legibles_inv[mes]
+                                                 for mes in ultimos_seis_meses])
+
+        ano_de_trabajo = st.selectbox(label='Año',
+                                      options=[2025, 2024])
+        seis_meses_atras = (dt.date(ano_de_trabajo,
+                                   meses_legibles[mes_de_trabajo],
+                                   15)
+                            - dt.timedelta(days = 30*7))
+        data = data.query(f'fecha_programada > "{seis_meses_atras.year}-{seis_meses_atras.month}-01"')
 
         st.divider()
 
-        st.title('Filtros 📥')
+    with st.expander('Configuración del reporte'):
+        st.subheader('Filtros 📥')
 
         lista_de_diagramas = ['Diagrama de Area',
                               'Mapa de Calor',
@@ -346,7 +411,7 @@ if uploaded_file is not None:
 
         #filtro por tipo de tarea (preventivo o correctivo)
         tipo_de_tarea_seleccionada = st.multiselect(label = 'Tipo de tareas',
-                                                    options = ['Preventivo', 'Correctivo'])
+                                                    options = ['PREVENTIVO', 'CORRECTIVO'])
 
         #check for filters
         if tipo_de_tarea_seleccionada != []:
@@ -376,129 +441,140 @@ if uploaded_file is not None:
 
         st.divider()
 
-        st.title('⚙️ Configuracion de Graficos️')
+        st.subheader('⚙️ Configuracion de Graficos️')
 
         #config unidad de tiempo en diagrama de tiempo
-        st.subheader("Diagrama de Area")
+        st.markdown("#### Diagrama de Area")
         timeframes = ['Semana', 'Mes', 'Q', 'Dia']
         timeFrameName = st.selectbox(label = 'Selecciona la escala de tiempo: ',
                                  options = timeframes)
         timeDict = {'Semana': 'week', 'Mes' : 'month', 'Q' : 'quarter', 'Dia': 'dayofyear'}
         timeFrame = timeDict[timeFrameName]
 
-        st.subheader("Diagrama de Pastel")
-        st.write('---pendiente por habilitar---')
-
-
     #Seleccion del tema
         selected_color_theme = 'reds'
 
-    st.title('Dashboard de Mantenimiento 🔩')
+    #KPIs
+    with st._main:
+        if 'KPIs' in diagramas_con_filtro:
+            selected_data = filtered_data
+        else:
+            selected_data = data
+
+        # DATOS DEL MES ANTERIOR
+        try:
+            if meses_legibles[mes_de_trabajo] != 1:
+                mes_anterior = selected_data.query(
+                    f'mes == {(meses_legibles[mes_de_trabajo] - 1)} & año == {ano_de_trabajo}')
+            else:
+                mes_anterior = selected_data.query(f'mes == 12 & año == {ano_de_trabajo - 1}')
+        except KeyError:
+            mes_anterior = None
+
+        # DATOS DEL MES ACTUAL
+        selected_data = selected_data.query(f'mes == {(meses_legibles[mes_de_trabajo])} & año == {ano_de_trabajo}')
+
+        val1 = float((selected_data['tipo_mantenimiento'] == 'PREVENTIVO').sum())
+        val2 = float((selected_data['tipo_mantenimiento'] == 'CORRECTIVO').sum())
+        val3 = float(selected_data['horas_reales'].sum())
+
+        # DIFERENCIA ENTRE MES ANTERIOR Y MES ACTUAL
+        try:
+            dif1 = val1 - (mes_anterior['tipo_mantenimiento'] == 'PREVENTIVO').sum()
+            dif2 = val2 - (mes_anterior['tipo_mantenimiento'] == 'CORRECTIVO').sum()
+            dif3 = round(val3 - mes_anterior['horas_reales'].sum(), 2)
+        except:
+            dif1 = None
+            dif2 = None
+            dif3 = None
+
+        kpi1, kpi2, kpi3 = st.columns(3)
+
+        kpi1.metric(label="Tareas Preventivas", value=val1, delta=dif1)
+        kpi2.metric(label="Tareas Correctivas", value=val2, delta=dif2)
+        kpi3.metric(label="Horas Totales", value=val3, delta=dif3)
+
     st.divider()
+
     col1, col2 = st.columns(2)
+    with col1:
+        # COLUMNA 1:
+        ### DIAGRAMA DE TIEMPO
 
-    # COLUMNA 1:
-    ### DIAGRAMA DE TIEMPO
+        if 'Diagrama de Area' in diagramas_con_filtro:
+            selected_data = filtered_data
+        else:
+            selected_data = data
 
-    if 'Diagrama de Area' in diagramas_con_filtro:
-        selected_data = filtered_data
-    else:
-        selected_data = data
+        st.write("Horas Trabajadas por Fecha")
 
-    col1.write("Horas Trabajadas por Fecha")
-    # horas de trabajo por fecha
-    diagrama_de_tiempo = generar_diagrama_tiempo(selected_data, timeFrame)
+        # horas de trabajo por fecha
+        diagrama_de_tiempo = generar_diagrama_tiempo(selected_data, timeFrame)
 
-    col1.altair_chart(diagrama_de_tiempo,
-                      use_container_width = True)
+        st.altair_chart(diagrama_de_tiempo,
+                          use_container_width = True)
 
-    ### DIAGRAMA DE CALOR
+        ### DIAGRAMA DE CALOR
 
-    # revisar filtro en mapa de calor
-    if 'Mapa de Calor' in diagramas_con_filtro:
-        selected_data = filtered_data
-    else:
-        selected_data = data
-    # creando figura
-    heatmap = generar_diagrama_de_calor(selected_data)
-    col1.altair_chart(heatmap,
-                      use_container_width=True)
-
-
+        # revisar filtro en mapa de calor
+        if 'Mapa de Calor' in diagramas_con_filtro:
+            selected_data = filtered_data
+        else:
+            selected_data = data
+        # creando figura
+        heatmap = generar_diagrama_de_calor(selected_data)
+        st.altair_chart(heatmap,
+                          use_container_width=True)
 
     # COLUMNA 2:
-    ### DIAGRAMA KPIS
+    with col2:
+        ### DIAGRAMA DE BARRAS
+        if 'Diagrama de Barras' in diagramas_con_filtro:
+            # datos con filtro arbitrario aplicado
+            selected_data = filtered_data
+        else:
+            selected_data = data
 
-    if 'KPIs' in diagramas_con_filtro:
-        selected_data = filtered_data
-    else:
-        selected_data = data
+        # DATOS DEL MES
+        selected_data = selected_data.query(f'mes == {(meses_legibles[mes_de_trabajo])} & año == {ano_de_trabajo}')
 
-        # datos del mes anterior
-    try:
-        mes_anterior = selected_data.query(f'mes == {(meses_legibles[mes_de_trabajo] - 1)}')
-    except KeyError:
-        mes_anterior = None
-
-    # ahora nos quedamos solo con aquellos del mes de trabajo actual
-    selected_data = selected_data.query(f'mes == {(meses_legibles[mes_de_trabajo])}')
-
-    val1 = float((selected_data['tipo_mantenimiento'] == 'PREVENTIVO').sum())
-    val2 = float((selected_data['tipo_mantenimiento'] == 'CORRECTIVO').sum())
-    val3 = float(selected_data['horas_reales'].sum())
-
-    try:
-        # datos del mes anterior
-        dif1 = val1 - (mes_anterior['tipo_mantenimiento'] == 'PREVENTIVO').sum()
-        dif2 = val2 - (mes_anterior['tipo_mantenimiento'] == 'CORRECTIVO').sum()
-        dif3 = round(val3 - mes_anterior['horas_reales'].sum(), 2)
-
-    except:
-        dif1 = None
-        dif2 = None
-        dif3 = None
-
-    kpi1, kpi2, kpi3 = col2.columns(3)
-
-    kpi1.metric(label = "Tareas Preventivas", value = val1, delta = dif1)
-    kpi2.metric(label = "Tareas Correctivas", value = val2, delta = dif2)
-    kpi3.metric(label = "Horas Totales", value = val3, delta = dif3)
-
-    col2.divider()
+        st.write('Horas por Trabajador')
+        diagrama_de_barras = generar_diagrama_de_barras(selected_data)
+        st.altair_chart(diagrama_de_barras,
+                          use_container_width = True)
+        #PIE CHART
+        agg = 'sum'
 
 
-    ### DIAGRAMA DE BARRAS
-    if 'Diagrama de Barras' in diagramas_con_filtro:
-        # datos con filtro arbitrario aplicado
-        selected_data = filtered_data
-    else:
-        selected_data = data
+        ### DIAGRAMA PIE CHART
+        if 'Diagrama de Pastel' in diagramas_con_filtro:
+            selected_data = filtered_data
+        else:
+            selected_data = data
 
-    # ahora nos quedamos solo con aquellos del mes de trabajo actual
-    selected_data = selected_data[selected_data['mes'] == meses_legibles[mes_de_trabajo]]
+        # ahora nos quedamos solo con aquellos del mes de trabajo actual
+        selected_data = selected_data.query(f'mes == {(meses_legibles[mes_de_trabajo])} & año == {ano_de_trabajo}')
 
-    col2.write('Horas por Trabajador')
+        pie_chart = generar_pie_chart(selected_data, 9)
 
-    #diagrama de barras
-    diagrama_de_barras = generar_diagrama_de_barras(selected_data)
-    col2.altair_chart(diagrama_de_barras,
-                      use_container_width = True)
-    #pie chart
-    agg = 'sum'
-
-
-    ### DIAGRAMA PIE CHART
-    if 'Diagrama de Pastel' in diagramas_con_filtro:
-        selected_data = filtered_data
-    else:
-        selected_data = data
-
-    selected_data = selected_data[selected_data['mes'] == meses_legibles[mes_de_trabajo]]
-
-    pie_chart = generar_pie_chart(selected_data, 9)
-
-    col2.write('Incidencias por Tarea')
-    col2.altair_chart(pie_chart, use_container_width = True)
+        st.write('Incidencias por Tarea')
+        st.altair_chart(pie_chart, use_container_width = True)
 
     st.divider()
+
+    with st.expander('Top 10 Tareas:'):
+        last_thing = data.query(f'mes=={meses_legibles[mes_de_trabajo]}')[
+            ['orden_de_trabajo',
+             'fecha_programada',
+             'costo_tarea',
+             'horas_reales',
+             'tipo_tarea',
+             'zona',
+             'equipo',
+             'componente',
+             'trabajador']].sort_values(
+            by=['costo_tarea'],
+            ascending=False).head(10)
+
+        st.dataframe(last_thing)
     st.caption('Apoyo visual para análisis de datos de mantenimiento. \nDiseñado para exponer resumen del mes.')
